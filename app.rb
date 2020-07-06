01372c8558cb849d360dba843eda16c4b79ebcb8
serj <seregaproger@yandex.ru>
2020-07-06 14:18:55 +0300

    qwery all Cars

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sqlite3'
 db = SQLite3::Database.new 'mydatabase.sqlite'
 
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Vaz', 777)"
+# db.execute "INSERT INTO Cars (Name, Price) VALUES ('Vaz', 777)"
+
+db.execute "SELECT * FROM Cars" do |car|
+	puts car
+	puts '------'	
+end
 
 db.close
